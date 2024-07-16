275a31a185af47e12f6d340bcc29140e0fce8a4d
Darepen <juliojosepedrena@gmail.com>
2024-07-16 11:49:41 +0800

    refactor(bonus-points): change structure of gallery

--- pages/bonus-points/assets/js/scripts.js
+++ pages/bonus-points/assets/js/scripts.js
@@ -1,25 +1,27 @@
-let currentIndex = 0;
+// JavaScript for slideshow functionality
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showImage(index) {
-    const images = document.querySelectorAll('.carousel-item');
-    if (index >= images.length) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = images.length - 1;
-    } else {
-        currentIndex = index;
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    const offset = -currentIndex * 100;
-    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
-
-function showNextImage() {
-    showImage(currentIndex + 1);
-}
-
-function showPrevImage() {
-    showImage(currentIndex - 1);
-}
-
-// Initial display
-showImage(currentIndex);
